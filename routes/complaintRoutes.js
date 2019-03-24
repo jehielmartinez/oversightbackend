8f25437545a92c32199e85527e25a653fff7d698
Jehiel Martinez <jehielmartinez@gmail.com>
2019-03-24 14:42:42 -0600

    cloudinary added

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const moment = require('moment');
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "images",
+    allowedFormats: ["jpg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }]
+});
+
+const parser = multer({ storage: storage });
 
 const {complaint} = require('../db/complaintModel');
 
@@ -29,6 +47,20 @@ let routes = (io) => {
             },(err) => {
                 res.status(400).send(err);
         });
+    });
+
+    router.post('/image', (req, res)=>{
+        let dataURI = req.body.dataURI;
+        let uploadStr = 'data:image/jpeg;base64'+ dataURI;
+
+        cloudinary.v2.uploader.upload(uploadStr, {
+            overwrite: true,
+            invalidate: true,
+            width: 810, height: 456, crop: "fill"
+        },
+            function (error, result) {
+                res.json(result);
+            });
     });
 
     return router;
