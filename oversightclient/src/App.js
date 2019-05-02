de805499f1111e37255e652ebac9b69b1bb54262
Jehiel Martinez <jehielmartinez@gmail.com>
2019-05-02 00:35:55 -0600

    react-ga added

--- oversightclient/src/App.js
+++ oversightclient/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Router from './components/Router/Router';
+import ReactGA from 'react-ga';
 
 class App extends Component {
 
+  initializeReactGA() {
+      ReactGA.initialize('UA-139384535-1');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+  }
   render() {
+    this.initializeReactGA()
     return (
       <Router/>
     );
