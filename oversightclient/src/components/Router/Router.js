92c8b56a6add1fcaf6d5e52a116222bd5845b384
Jehiel Martinez <jehielmartinez@gmail.com>
2019-05-09 12:15:32 -0600

    test page added

--- oversightclient/src/components/Router/Router.js
+++ oversightclient/src/components/Router/Router.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import {Redirect, Route, Switch, BrowserRouter} from 'react-router-dom';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import LandingPage from '../LandingPage/LandingPage';
+import Testpage from '../Testpage';
 
 class Router extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Redirect exact from='/' to='/landing'/>
                     <Route
                         exact
-                        path='/landing'
+                        path='/'
                         component={LandingPage}
+                   />
+                   <Route
+                        exact
+                        path='/test'
+                        component={Testpage}
                    />
                 </Switch>
             </BrowserRouter>
---
+++ oversightclient/src/components/Testpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Testpage extends Component {
+    render() {
+        return (
+            <div>
+                This ts the Test component
+            </div>
+        );
+    }
+}
+
+export default Testpage;
